9597b285d7920749c4a8a23bf52d0ca554caf3f2
Handballfreak <56076969+Handballfreak@users.noreply.github.com>
2020-02-17 18:15:56 +0100

    Vorlage_pfad versuch

--- build/Datei_Import.py
+++ build/Datei_Import.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import os
 
-def getpfad():
+def get_pfad():
     pfad = os.getcwd()
-    #pfad=os.path.abspath(".")
-    print( pfad)
+    #print(pfad)
+
+def get_vorlage_pfad():
+    pfad = get_pfad()
+    pfad=str(pfad)
+    pfad_split = pfad.split("W")
+    print(pfad_split)
