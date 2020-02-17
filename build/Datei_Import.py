070443a0f06e51a5293de27c971edd5fa14edc9d
Handballfreak <56076969+Handballfreak@users.noreply.github.com>
2020-02-17 17:41:59 +0100

    import

---
+++ build/Datei_Import.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import os
+
+def getpfad():
+    #pfad = os.getcwd()
+    pfad=os.path.abspath(".")
+    print(pfad,___file__)
--- build/Main.py
+++ build/Main.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import Datei_Import
 
-activities = pd.read_csv("activities")
-print(activities)
+activities = pd.read_csv("C:\\Users\\hdz18\PycharmProjects\\WithingsData2\\src\\aggregates_calories_earned.csv")
+#print(activities)
+
+print("/n")
+print(Datei_Import.getpfad)
+print("/n")
+print(Datei_Import.getpfad)
