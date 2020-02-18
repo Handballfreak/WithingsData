8a734b53e3729d75bf383d4550b1d6c5c6a17c9b
Handballfreak <56076969+Handballfreak@users.noreply.github.com>
2020-02-18 10:02:37 +0100

    Dateien einlese Funktion

--- build/Datei_Import.py
+++ build/Datei_Import.py
@@ -22,3 +22,26 @@ def datei_einlesen(dateiname):
     datei = pd.read_csv(vorlage + dateiname)
     return datei
 
+
+def get_dataframe():
+    activities = datei_einlesen("activities.csv")
+    calories_earned = datei_einlesen("aggregates_calories_earned.csv")
+    calories_passive = datei_einlesen("aggregates_calories_passive.csv")
+    distance = datei_einlesen("aggregates_distance.csv")
+    elevation = datei_einlesen("aggregates_elevation.csv")
+    steps = datei_einlesen("aggregates_steps.csv")
+    sleep = datei_einlesen("sleep.csv")
+    raw_altitude = datei_einlesen("raw_tracker_altidude.csv")
+    raw_calories_earned = datei_einlesen("raw_tracker_calories_earned.csv")
+    raw_distance = datei_einlesen("raw_tracker_distance.csv")
+    raw_elevation = datei_einlesen("raw_tracker_elevation.csv")
+    raw_gps_speed = datei_einlesen("raw_tracker_gps-speed.csv")
+    raw_horizontal_radius = datei_einlesen("raw_tracker_horizontal-radius.csv")
+    raw_hr = datei_einlesen("raw_tracker_hr.csv")
+    raw_lap_pool = datei_einlesen("raw_tracker_lap-pool.csv")
+    raw_latitude = datei_einlesen("raw_tracker_latitude.csv")
+    raw_longtitude = datei_einlesen("raw_tracker_longtitude.csv")
+    raw_sleep_state = datei_einlesen("raw_tracker_sleep-state.csv")
+    raw_steps = datei_einlesen("raw_tracker_steps.csv")
+    raw_vertical_radius = datei_einlesen("raw_tracker_vertical-radius.csv")
+
--- build/Main.py
+++ build/Main.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import Datei_Import
 
-
-
 activities = Datei_Import.datei_einlesen("activities.csv")
-print(activities)
+#print(activities)
