167336972c88ec4c621ea9ab6493350dec3c853f
Handballfreak <56076969+Handballfreak@users.noreply.github.com>
2020-02-22 22:19:46 +0100

    Graphen zeigen

--- build/Main.py
+++ build/Main.py
@@ -2,10 +2,10 @@
 import Datei_Import
 import Visualization
 
-# Visualization.distance_graph()
-# Visualization.step_graph()
+Visualization.distance_graph()
+Visualization.step_graph()
 activities, calories_earned, calories_passive, distance, elevation, steps, sleep, raw_altitude, raw_calories_earned, raw_distance, raw_elevation, raw_gps_speed, raw_horizontal_radius, raw_hr, raw_lap_pool, raw_latitude, raw_longtitude, raw_sleep_state, raw_steps, raw_vertical_radius = Datei_Import.get_dataframe()
 
 activities = Datei_Import.clean_activities(activities)
 print(activities)
-# print(Datei_Import.help_clean_activities("calories", ["data", "calories":]))
+
