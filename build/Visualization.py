97097a9dddf1541a2ced91cc842dc27ceb74b6f1
Frédéric <56076969+handballfreak@users.noreply.github.com>
2022-01-15 11:22:24 +0100

    kleine änderungen

--- build/Visualization.py
+++ build/Visualization.py
@@ -9,7 +9,6 @@ from datetime import date
 from tkinter import *
 from tkinter.messagebox import showerror
 
-
 activities, calories_earned, calories_passive, distance, elevation, steps, sleep, raw_altitude, \
 raw_calories_earned, raw_distance, raw_elevation, raw_gps_speed, raw_horizontal_radius, raw_hr, \
 raw_lap_pool, raw_latitude, raw_longtitude, raw_sleep_state, raw_steps, \
